b11805d95e0da986281af4409ea853e4b954d9c7
chowkl <chowkl-wm19@studnt.tarc.edu.my>
2020-07-14 16:51:34 +0800

    commit by chowkl

--- app/src/main/java/com/example/android/DroidCafeInput/OrderActivity.java
+++ app/src/main/java/com/example/android/DroidCafeInput/OrderActivity.java
@@ -18,6 +18,7 @@ package com.example.android.DroidCafeInput;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -102,5 +103,18 @@ public class OrderActivity extends AppCompatActivity implements
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), getString(R.string.datepicker));
+    }
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string +
+                "/" + day_string + "/" + year_string);
+        Toast.makeText(this, getString(R.string.date) + dateMessage,
+                Toast.LENGTH_SHORT).show();
     }
 }
